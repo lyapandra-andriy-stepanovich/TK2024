20d4f0ea1a1b4bf621e6d6ad4d8834b03ed09585
LucarisVey <ruslanp756@gmail.com>
2024-02-26 12:20:11 +0200

    Minor changes

--- src/tk/cs/lr/Main.java
+++ src/tk/cs/lr/Main.java
@@ -1,7 +1,8 @@
 package tk.cs.lr;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("HW");
     }
 }
